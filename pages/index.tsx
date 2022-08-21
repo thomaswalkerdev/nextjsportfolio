0abafbbcb68627a1d5c410b826e00ce9e540d33e
Tom Walker <thomaswalkerdev@gmail.com>
2022-08-21 21:08:56 +0100

    add title to home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,15 @@
+import { PageHeader } from "grommet";
 import React from "react";
 
 function Home() {
-  return <div>HomePageChanged</div>;
+  return (
+    <div>
+      <PageHeader
+        title="Thomas Walker"
+        subtitle="Full Stack Software Developer"
+      />
+    </div>
+  );
 }
 
 export default Home;
