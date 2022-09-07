7da2a9bf70f050e7e8b4adf451e1e62cb94076aa
Tom Walker <thomaswalkerdev@gmail.com>
2022-09-07 19:16:11 +0100

    missed a save file

---
+++ services/projects.service.ts
@@ -0,0 +1,34 @@
+export const ProjectsService = {
+  getProjects() {
+    return [
+      {
+        header: "Project 1",
+        body: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores qui fugit dolorem magni aut labore, incidunt magnam error earum recusandae assumenda vel temporibus, tempora enim ullam perferendis? Perspiciatis, dolor saepe! Nobis vero ea accusamus id, magnam praesentium quidem aut ullam possimus cupiditate! Ut asperiores, soluta amet vel ad, ab, ipsam eos corporis quam voluptatum debitis vitae impedit quis ipsum at?",
+      },
+      {
+        header: "Project 2",
+        body: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores qui fugit dolorem magni aut labore, incidunt magnam error earum recusandae assumenda vel temporibus, tempora enim ullam perferendis? Perspiciatis, dolor saepe! Nobis vero ea accusamus id, magnam praesentium quidem aut ullam possimus cupiditate! Ut asperiores, soluta amet vel ad, ab, ipsam eos corporis quam voluptatum debitis vitae impedit quis ipsum at?",
+      },
+      {
+        header: "Project 3",
+        body: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores qui fugit dolorem magni aut labore, incidunt magnam error earum recusandae assumenda vel temporibus, tempora enim ullam perferendis? Perspiciatis, dolor saepe! Nobis vero ea accusamus id, magnam praesentium quidem aut ullam possimus cupiditate! Ut asperiores, soluta amet vel ad, ab, ipsam eos corporis quam voluptatum debitis vitae impedit quis ipsum at?",
+      },
+      {
+        header: "Project 4",
+        body: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores qui fugit dolorem magni aut labore, incidunt magnam error earum recusandae assumenda vel temporibus, tempora enim ullam perferendis? Perspiciatis, dolor saepe! Nobis vero ea accusamus id, magnam praesentium quidem aut ullam possimus cupiditate! Ut asperiores, soluta amet vel ad, ab, ipsam eos corporis quam voluptatum debitis vitae impedit quis ipsum at?",
+      },
+      {
+        header: "Project 5",
+        body: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores qui fugit dolorem magni aut labore, incidunt magnam error earum recusandae assumenda vel temporibus, tempora enim ullam perferendis? Perspiciatis, dolor saepe! Nobis vero ea accusamus id, magnam praesentium quidem aut ullam possimus cupiditate! Ut asperiores, soluta amet vel ad, ab, ipsam eos corporis quam voluptatum debitis vitae impedit quis ipsum at?",
+      },
+      {
+        header: "Project 6",
+        body: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores qui fugit dolorem magni aut labore, incidunt magnam error earum recusandae assumenda vel temporibus, tempora enim ullam perferendis? Perspiciatis, dolor saepe! Nobis vero ea accusamus id, magnam praesentium quidem aut ullam possimus cupiditate! Ut asperiores, soluta amet vel ad, ab, ipsam eos corporis quam voluptatum debitis vitae impedit quis ipsum at?",
+      },
+      {
+        header: "Project 7",
+        body: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores qui fugit dolorem magni aut labore, incidunt magnam error earum recusandae assumenda vel temporibus, tempora enim ullam perferendis? Perspiciatis, dolor saepe! Nobis vero ea accusamus id, magnam praesentium quidem aut ullam possimus cupiditate! Ut asperiores, soluta amet vel ad, ab, ipsam eos corporis quam voluptatum debitis vitae impedit quis ipsum at?",
+      },
+    ];
+  },
+};
